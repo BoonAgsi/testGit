9516a6b250c5154700661007e5f8722f32b93ea4
BoonAgsi <boonagsi@gmail.com>
2022-11-01 13:11:57 +0800

    This is branch B

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello This is branch b',
+      title: 'Hello This is branch b  bbb bb b bb',
       theme: ThemeData(
         // This is the theme of your application.
         //
